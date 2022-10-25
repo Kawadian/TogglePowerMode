7198932a3443d317e572621ff6daeeadf0bccc5e
Kawadian <hanta_sho@icloud.com>
2022-10-25 16:04:17 +0900

    using root

---
+++ TogglePowerMode_root.sh
@@ -0,0 +1,11 @@
+status="`pmset -g |grep lowpowermode`"
+status="`echo $status | tr -d ' '`"
+LowPower="lowpowermode1"
+Performance="lowpowermode0"
+if [ $status = "$LowPower" ]; then
+    pmset -a lowpowermode 0 > /dev/null 2>&1
+    echo "\n低電力モードがオフになりました"
+elif [ $status = "$Performance" ]; then
+    pmset -a lowpowermode 1 > /dev/null 2>&1
+    echo "\n低電力モードがオンになりました"
+fi
